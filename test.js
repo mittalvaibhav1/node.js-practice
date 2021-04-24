a500f439c3b888c52e1561079e490cca65f3e107
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-25 02:07:21 +0530

    Mongoose Subdocuments

--- test.js
+++ test.js
@@ -12,10 +12,23 @@ connect.then((db) => {
     })
     .then((dish) => {
         console.log(dish);
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: "Updated test" } 
+        }, { 
+            new: true 
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling',
+            author: "Leo"
+        })
+        return dish.save();
+    })
+    .then((dish) => {  
+        console.log(dish);  
         return Dishes.remove({});
     })
     .then(() => {
